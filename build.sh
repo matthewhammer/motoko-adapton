629f1733a15805d7b52aa5e1366ac1939cd79314
Matthew Hammer <matthew@dfinity.org>
2020-04-24 10:45:40 -0600

    remove dfx

--- build.sh
+++ build.sh
@@ -1,5 +1,3 @@
 moc `./vessel sources` --package adapton src -r test/test.mo &&\
 moc `./vessel sources` --package adapton src --idl test/Calc.mo &&\
-moc `./vessel sources` --package adapton src -c test/Calc.mo &&\
-cp Calc.wasm dfxproj/canisters/dfxproj/main.wasm
-
+moc `./vessel sources` --package adapton src -c test/Calc.mo
